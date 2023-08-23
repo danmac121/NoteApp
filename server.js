d94834acc5e9e3a3f8ef50ef33f98cdd6cb961b5
danmac121 <danmacdonald121@gmail.com>
2023-08-23 15:52:10 -0400

    set up end points for get on api/notes and notes. set up post end point for api/notes. notes with ids can be retrieved and displayed by clicking on them

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const path = require('path')
+const PORT = 3001;
+const fs = require('fs');
+const app = express();
+const uuid = require('./helpers/uuid');
+const notes = require('./db/db.json')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+const noteFile = './db/db.json'
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+app.get('/api/notes', (req, res) => {
+  fs.readFile(noteFile, 'utf8', (err, data) => {
+    console.log(data)
+    if (err) {
+      console.error('Error reading notes:', err);
+      res.status(500).json('Error reading notes');
+      return;
+    }
+
+    else {
+
+     const parsedNotes = JSON.parse(data);
+      res.json(parsedNotes);
+    } 
+  });
+});
+
+
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+  res.status(200).json(notes);
+});
+
+
+app.post('/api/notes', (req, res) => {
+
+  console.info(`${req.method} request received to add a note`);
+
+
+  const { title, text } = req.body;
+
+
+  if (title && text) {
+
+    const newNote = {
+      title, 
+      text,
+      id: uuid(),
+    };
+    notes.push(newNote)
+    const noteString = JSON.stringify(notes, null, 2);
+    fs.writeFile(`./db/db.json`, noteString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `Note for ${noteString} has been written to JSON file`
+          )
+    );
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
