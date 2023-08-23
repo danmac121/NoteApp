bc9cfb961bf435505ce071315bd2b7d660770dc7
danmac121 <danmacdonald121@gmail.com>
2023-08-23 16:03:04 -0400

    added delete function

--- server.js
+++ server.js
@@ -30,6 +30,23 @@ app.get('/api/notes', (req, res) => {
   });
 });
 
+app.delete("/api/notes/:id", function (req, res) {
+  let noteFile = path.join(__dirname, "/db/db.json");
+  for (let i = 0; i < notes.length; i++) {
+      if (notes[i].id == req.params.id) { 
+          notes.splice(i, 1);
+          break;
+      }
+  }
+  fs.writeFileSync(noteFile, JSON.stringify(notes), function (err) {
+      if (err) {
+          return console.log(err);
+      } else {
+          console.log("The note was deleted.");
+      }
+  });
+  res.json(notes);
+});
 
 app.get('/api/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
@@ -43,13 +60,8 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
 
   console.info(`${req.method} request received to add a note`);
-
-
   const { title, text } = req.body;
-
-
   if (title && text) {
-
     const newNote = {
       title, 
       text,
